fc292566d8eca6b71b4578a96d9e22adfe41117f
Aditya Solge <aditya.solge001@umb.edu>
2026-01-28 13:54:18 +0530

    useState examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import Student from "./pages/examples/Student";
-import Student1 from "./pages/examples/Student1";
-import Student2 from "./pages/examples/Student2";
+import Student5 from "./pages/examples/Student5";
 
 function App() {
     return (
         <>
-            <h1>Welcome to Expense App</h1>
-            <Student />
-            
-            <Student1 />
-            
-            <Student1 name="John" rollNumber={20} />
-
-            <Student2 name="Sam" rollNumber={40} />
+            <Student5 />
         </>
     );
 }
